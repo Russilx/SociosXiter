aa6483455d6ae435078ed3002eb1c9a5e2dd6b28
Russilx <sanmartinesteban123@gmail.com>
2026-08-16 17:15:07 -0300

    Delete config.js

--- config.js
+++
@@ -1,4 +0,0 @@
-// Reemplazá estos dos valores con los de tu proyecto Supabase:
-// Panel de Supabase → Project Settings → API
-const SUPABASE_URL = "https://gtlgcvozyiqjgvscaxhw.supabase.co";
-const SUPABASE_ANON_KEY = "sb_publishable_Qznt2zN6pW1MxgeLhSlbRA_gNam6nxy";
