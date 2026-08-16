0f753dc42e9d0da313ffc7fe74739695c25e6070
Russilx <sanmartinesteban123@gmail.com>
2026-08-16 17:15:40 -0300

    Delete music-config.js

--- music-config.js
+++
@@ -1,4 +0,0 @@
-// Lista de canciones de fondo para la pantalla de login.
-// Poné acá los nombres de archivo. Se van a ir reproduciendo en
-// orden y, al terminar la última, vuelve a empezar de la primera.
-const MUSIC_TRACKS = ["musica2.mp3", "musica1.mp3"];
